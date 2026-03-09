390f7ed0a28ff6a65bafd6ee7718c742476c2ab4
Nishit Patel <pnishit238@gmail.com>
2026-03-09 12:22:57 +0530

    Explored how to use pyproject.toml

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/companies/{company_id}")
+async def get_company_by_id(company_id: int):
+    return {"company_id": company_id, "name": f"Company {company_id}"}
+
