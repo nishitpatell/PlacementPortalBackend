955f383e978e1306ac56cb4a5f3b6fe4d5d2c726
Nishit Patel <pnishit238@gmail.com>
2026-03-23 09:45:41 +0530

    Set up Docker configuration with multiple API instances and load balancer; added environment variables for instance names and database URL; updated FastAPI health check to include instance information.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 import tomllib
 from fastapi import FastAPI
 from app.routers import company_router
@@ -5,6 +6,7 @@ from app.routers import company_router
 with open("pyproject.toml", "rb") as f:
     project_metadata = tomllib.load(f)["project"]
 
+INSTANCE_NAME = os.getenv("INSTANCE_NAME", "Unknown Instance")
 
 app = FastAPI(
     title = project_metadata.get("name", "Placement Portal API"),
@@ -16,4 +18,8 @@ app.include_router(company_router.router)
 
 @app.get("/")
 def health_check():
-    return {"status": "ok", "message": "Placement Portal API is running"}
+    return {
+        "status": "ok", 
+        "message": "Placement Portal API is running",
+        "instance_handled_by": INSTANCE_NAME 
+    }
--- app/routers/company_router.py
+++ app/routers/company_router.py
@@ -1,7 +1,4 @@
-from fastapi import APIRouter, Depends, status
-from sqlalchemy.orm import Session
-from app.core.database import get_db
-from app.repositories import company_repository
+from fastapi import APIRouter, Depends, status, Query
 from app.schemas.company_schema import CompanyResponse, CompanyCreate
 from app.services.company_service import CompanyService
 
@@ -17,7 +14,9 @@ def create_company_endpoint(company_in: CompanyCreate, _companyservice: CompanyS
     return _companyservice.create_company(company_data=company_in)
 
 @router.get("", response_model=list[CompanyResponse])
-def get_companies_endpoint(skip: int = 0, limit: int = 100, _companyservice: CompanyService = Depends()):
+def get_companies_endpoint(skip: int = Query(0, ge=0, description="Number of records to skip for pagination"), 
+                           limit: int = Query(100, ge=1, le=100, description="Maximum number of records to return"), 
+                           _companyservice: CompanyService = Depends()):
     # Fetch companies from the repository layer using the DB session
     return  _companyservice.get_companies( skip=skip, limit=limit)
     
